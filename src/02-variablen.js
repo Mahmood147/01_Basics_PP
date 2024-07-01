7dadc06396be5d5bdd352c5ceac7567e711192bb
Mahmood147 <160109243+Mahmood147@users.noreply.github.com>
2024-07-01 10:50:58 +0000

    Variablen

--- src/02-variablen.js
+++ src/02-variablen.js
@@ -10,8 +10,32 @@
 
 
 
-let firstName ="Mahmood"; // Deklaration ( Definiton)
+/**let firstName ="Mahmood"; // Deklaration ( Definiton)
 console.log(firstName); // Ausgabe
 let familyName = "Abdal";
 console.log (familyName)
 console.log ("hi ich bin "+ firstName + " "+ familyName)
+*/
+
+let test; // Deklaration
+
+
+console.log ("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------")
+
+test = "hi"; // String
+console.log ("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------")
+
+
+test = 2;
+console.log ("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------")
+
+test = false;
+console.log ("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------")
