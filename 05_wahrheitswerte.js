bc4044420303c02be69e5bdc0079030cb9096e8e
Mahmood147 <160109243+Mahmood147@users.noreply.github.com>
2024-07-03 09:11:55 +0000

    if else

--- 05_wahrheitswerte.js
+++ 05_wahrheitswerte.js
@@ -6,12 +6,55 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
+/**  Berechnung Alter version 1
 let year = 2024;
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+*/
+
+let date = new Date();
+console.log("Datum " + date);
+console.log("Jahr " + date.getFullYear());
+let year = date.getFullYear();
+
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+// Deklaration 
+let istJahnOlder;  // Is John older? --> y/n --> true or false 
+istJahnOlder = (ageJohn > ageMark); //Test
+istMarkOlder = (ageJohn < ageMark)
+console.log (istJahnOlder);
+console.log("---------------")
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+//if(true)
+//if(false)
+//if(isJahnOlder)
+//{
+//        console.log("John is älter")
+//}
+
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+if (istMarkOlder) 
+{
+    console.log("John is älter")
+    
+} 
+else 
+{
+    console.log("John is jünger")
+}
