2d8a19af374fe96fff3814362c2a0cfdba6cf12e
Mahmood147 <160109243+Mahmood147@users.noreply.github.com>
2024-07-04 10:04:48 +0000

    Funktionalitäten

--- 08_funktionen_I.js
+++ 08_funktionen_I.js
@@ -26,11 +26,47 @@ function ausgabeNamen() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeName2("Adrian"); // Argument --> Daten für Parameter 
-ausgabeName2("Mahmood")
-ausgabeName2("Leyla")
+ 
+ //ausgabeName2("Leyla") // Argument --> Daten für Parameter 
 
 function ausgabeName2(firstName)  // Parameter
 {
     console.log("Hallo," + firstName + "!")
 }
+
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+//const prompt = require('prompt-sync')({sigint: true});
+//ausgabeNamenParams(prompt("Vorname?: "),prompt("Name?: "));
+
+
+function ausgabeNamenParams(firstName, FamilyName) 
+{
+    console.log("Hallo, " + firstName + " " + FamilyName + "!")
+}
+
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ ausgabeNamenSRP("Max","Mütze");
+
+
+
+function ausgabeNamenSRP(firstName, FamilyName) 
+{
+   // 1. Funktionalität : string composing
+
+   const GAP = " "
+    let outputStr = console.log("Hallo, " + firstName + " " + FamilyName + "!")
+
+
+   // 2. Funktionalität : string output
+
+    console.log(outputStr)
+}
+
