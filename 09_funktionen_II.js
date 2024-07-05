7f1344833aca7a1c93fb7489eee0d404fd6ab2f6
Mahmood147 <160109243+Mahmood147@users.noreply.github.com>
2024-07-05 10:15:47 +0000

    Taschenrechner

--- 09_funktionen_II.js
+++ 09_funktionen_II.js
@@ -5,18 +5,64 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung :  
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check
+3. Fkt. Grundrechenarten : check
 4. Ausgabe in Konsole : check
 */
+const prompt = require("prompt-sync")({sigint: true});
+
+
+startApp()
+function startApp() {
+	output(calculator(getNum1(),getOP(),getNum2()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?: "));
+}
+function getOP() {
+	return prompt("OP?: ");
+}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?: "));
+}
+
+
+
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+ //output(calculator(3,2,"+"));
+ //output(calculator(3,2,"-"));
+ //output(calculator(3,2,"*"));
+ //output(calculator(3,2,":"));
+ //output(calculator(3,2,"/"));
+ //output(calculator(3,0,"/"));
+ //output(calculator(3,2,"#?!"));
+function calculator(a,op,b) {
+	
+	switch (op) {
+		case "+":
+			return add(a,b);
+		case "-":
+			return subtract(a,b)
+		case "*":
+			return multiply(a,b)
+		case "/":
+		case ":":
+			return divide(a,b)
+		default:
+			return "fehler"
+} }
 
 // module: division a / b |  test:
 // output(divide(4,2));
 // output(divide(3,2));
 // output(divide(3,-2));
 // output(divide(0,2));
- //output(divide(3,0));
- //output(divide(0,0));
+//output(divide(3,0));
+//output(divide(0,0));
 
 function divide(a,b) {
 	if (b != 0) {
@@ -65,4 +111,3 @@ function add(a,b) {
 function output(outputData) {
 	console.log(outputData);
 }
-  
